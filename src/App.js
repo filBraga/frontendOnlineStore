6e3308a76d3f76f0f56946c96ce999fb85fa8370
Diego Dmitry <diegodmitry@hotmail.com>
2022-01-19 16:27:27 -0300

    1. Implemente o módulo de acesso à api do Mercado Livre

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
-function App() {
+// getCategories();
+getProductsFromCategoryAndQuery(getCategories());
+class App extends React.Component {
   // First commit
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
