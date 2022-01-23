c3eb700be5b452f1192f1dbec0ccedaa60751953
Filipe <filipe.b.braga@gmail.com>
2022-01-23 11:12:01 -0300

    teste do 1 ao 5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Searchbar from './components/Searchbar';
-import Content from './components/Content';
+// import Content from './components/Content';
 import MainPage from './pages/MainPage';
 import './App.css';
 
@@ -12,10 +12,10 @@ export default class App extends Component {
       <BrowserRouter>
         <div className="root">
           <Route path="/" component={ MainPage } />
-          <Sidebar />
+          {/* <Sidebar /> */}
           <div className="searchNContent">
-            <Searchbar />
-            <Content />
+            {/* <Searchbar /> */}
+            {/* <Content /> */}
           </div>
         </div>
       </BrowserRouter>
